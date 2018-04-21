9979cbd68a83d0de1bcb82243ee9ba4716142bf7
augusto <augustomafra1@gmail.com>
2018-04-21 15:07:57 -0300

    Criando getRows e getCols

--- Matriz.java
+++ Matriz.java
@@ -1,6 +1,18 @@
 public class Matriz {
-    public static void main(String[] args) {
-        System.out.println("Hello There\n");
+
+    public Matriz(int ncol, int nrow) {
+        this.ncol = ncol;
+        this.nrow = nrow;
     }
+
+    public int getRows() { return nrow; }
+    public int getCols() { return ncol; }
+
+    public static void main(String[] args) {
+        Matriz m = new Matriz(1,2);
+        System.out.println("rows = " + m.getRows());
+        System.out.println("columns = " + m.getCols());
+    }
+
     private int ncol, nrow;
 }
