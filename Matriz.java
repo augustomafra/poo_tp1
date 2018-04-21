3ae56916038c3855003701b71bd59ec72323ae08
augusto <augustomafra1@gmail.com>
2018-04-21 15:24:20 -0300

    Checar erro no construtor

--- Matriz.java
+++ Matriz.java
@@ -1,6 +1,12 @@
 public class Matriz {
 
     public Matriz(int ncol, int nrow) {
+        if (ncol <= 0) {
+            throw new IndexOutOfBoundsException(Integer.toString(ncol));
+        }
+        if (nrow <= 0) {
+            throw new IndexOutOfBoundsException(Integer.toString(nrow));
+        }
         this.ncol = ncol;
         this.nrow = nrow;
     }
@@ -12,6 +18,9 @@ public class Matriz {
         Matriz m = new Matriz(1,2);
         System.out.println("rows = " + m.getRows());
         System.out.println("columns = " + m.getCols());
+
+        //Matriz wrong_m1 = new Matriz(1, -2);
+        Matriz wrong_m2 = new Matriz(0, -2);
     }
 
     private int ncol, nrow;
